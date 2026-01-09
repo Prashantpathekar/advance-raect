6a8b7fc193af57ab82d1bc131935267f58e8b5a3
Prashantpathekar <prashantpathekar538@gmail.com>
2026-01-10 00:07:31 +0530

    prashant

--- React_router_dom/src/Layout.jsx
+++ React_router_dom/src/Layout.jsx
@@ -10,14 +10,10 @@ const Layout = () => {
                         <li><Link to="/about" className='p-4 hover:bg-green-600'>about</Link></li>
                         <li><Link to="/services" className='p-4 hover:bg-green-600'>services</Link></li>
                         <li><Link to="/contact" className='p-4 hover:bg-green-600'>contact</Link></li>
-                        <li><Link to="/shreya" className='p-4 hover:bg-pink-600 -600'>Shreya</Link></li>
                          <li><Link to="/prashant" className='p-4 hover:bg-pink-600 -600'>prashant</Link></li>
                          <img src="vite.svg" alt="" />
                         <img src="vite.svg" alt="" />
-                   
-                        
-                        
-         </ul>
+                       </ul>
         </nav> 
              <button className='hover:bg-amber-400 border p-2 rounded-2xl text-blue-400 bg-amber-400 font-bold outline-black'>Login</button>
              </header>
